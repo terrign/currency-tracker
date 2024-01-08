106fdba527886d261ed40480786d2013cb53f93a
terrign <vajipi@gmail.com>
2024-01-08 21:24:33 +0300

    test: app context

--- src/context/App/hook.tsx
+++ src/context/App/hook.tsx
@@ -5,7 +5,7 @@ import AppContext from './App.context';
 const useAppContext = () => {
   const ctx = useContext(AppContext);
 
-  if (!ctx.preferredCurrency === null || !ctx.dispatch === null) {
+  if (ctx === undefined) {
     throw new Error('useAppContextHook must be used inside App.provider');
   }
   return ctx;
